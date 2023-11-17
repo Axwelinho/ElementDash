ab1514bcd2342bce8df6d05e846d6ad4baff4e8f
axel.Axwelinho <axel.rosatimarzetti@gmail.com>
2023-11-17 20:54:46 +0100

    //

--- Plugins/AlertFPS/Source/AlertFPS/AlertFPS.Build.cs
+++ Plugins/AlertFPS/Source/AlertFPS/AlertFPS.Build.cs
@@ -36,6 +36,7 @@ public class AlertFPS : ModuleRules
 			{
 				"CoreUObject",
 				"Engine",
+				"InputCore",
 				"Slate",
 				"SlateCore",
 				// ... add private dependencies that you statically link with here ...	
--- Plugins/AlertFPS/Source/AlertFPS/Private/AlertFPS.cpp
+++ Plugins/AlertFPS/Source/AlertFPS/Private/AlertFPS.cpp
@@ -7,6 +7,7 @@
 void FAlertFPSModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	AddToolbarExtension();
 }
 
 void FAlertFPSModule::ShutdownModule()
@@ -15,6 +16,10 @@ void FAlertFPSModule::ShutdownModule()
 	// we call this function before unloading the module.
 }
 
+void FYourPluginModule::AddToolbarExtension()
+{
+	FToolBarBuilder ToolbarBuilder(...); // Configurez votre barre d'outils
+	ToolbarBuilder.AddToolBarBut
 #undef LOCTEXT_NAMESPACE
 	
 IMPLEMENT_MODULE(FAlertFPSModule, AlertFPS)
